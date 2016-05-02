f7fade36b0ab43f3f6b6521bca957a33c479f9c8
BlinkBP <jan.kominczyk@gmail.com>
2016-05-02 21:21:29 +0000

    Further changes to GUI
    Initial proccess of creating final command to run in

--- AMPLauncher.py
+++ AMPLauncher.py
@@ -8,6 +8,9 @@ from subprocess import Popen, PIPE
 class AMPLauncher(Gtk.Window):
 
     def __init__(self):
+        self.fullscreenToggle = False
+        self.lavdoptsToggle = False
+        self.lavdoptsThreads = 1
         Gtk.Window.__init__(self, title="AMPLauncher")
         
         self.mainBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
@@ -17,15 +20,22 @@ class AMPLauncher(Gtk.Window):
         self.set_up_interface()
 
     def set_up_interface(self):
-        #Filepath entry
+        #Filepath
         self.filePathEntry = Gtk.Entry()
-        self.fileBox = Gtk.Box(homogeneous=True,spacing=6)
-        self.fileBox.add(self.filePathEntry)
-        #File dialog button
         self.chooseFileBtn = Gtk.Button(label="Open File")
-        self.chooseFileBtn.connect("clicked", self.on_open_file_clicked)
-        self.fileBox.add(self.chooseFileBtn)
-        self.mainBox.add(self.fileBox)
+        self.fileGrid = Gtk.Grid()
+        self.filePathEntry.set_hexpand(True)
+        self.fileGrid.attach(self.filePathEntry,
+                             0,
+                             0,
+                             2,
+                             1)
+        self.fileGrid.attach_next_to(self.chooseFileBtn,
+                                     self.filePathEntry,
+                                     Gtk.PositionType.RIGHT,
+                                     1,
+                                     1)
+        self.mainBox.add(self.fileGrid)
         #ao/vo combo boxes
         self.voLabel = Gtk.Label("Select video driver:")
         self.aoLabel = Gtk.Label("Select audio driver:")
@@ -40,19 +50,43 @@ class AMPLauncher(Gtk.Window):
         self.comboBox.add(self.aoLabel)
         self.comboBox.add(self.aoComboBox)
         self.mainBox.add(self.comboBox)
-        #lavdopts
-        self.lavdoptsLabel = Gtk.Label("lavdopts (MPEG-1/2 and H.264 only):")
+        #other options
+        self.otherLabel = Gtk.Label("Other options:")
         self.fsCheckBtn = Gtk.CheckButton("Fullscreen")
-        self.fsCheckBtn.connect("toggled", self.on_fullscreen_toggle)
+        self.useLavdoptsBtn = Gtk.CheckButton("Use lavdopts")
+        self.threadsLabel = Gtk.Label("Threads(MPEG-1/2 and H.264 only):")
         self.threadsSpinBtn = Gtk.SpinButton()
         adjustment = Gtk.Adjustment(1, 1, self.get_threads(), 1)
         self.threadsSpinBtn.set_adjustment(adjustment)
-        self.lavdoptsBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
+        self.threadsSpinBtn.set_sensitive(False)
+        self.otherBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
                                    spacing=6)
-        self.lavdoptsBox.add(self.lavdoptsLabel)
-        self.lavdoptsBox.add(self.fsCheckBtn)
-        self.lavdoptsBox.add(self.threadsSpinBtn)
-        self.mainBox.add(self.lavdoptsBox)
+        self.threadsBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL,
+                                  spacing=6)
+        self.otherBox.add(self.otherLabel)
+        self.otherBox.add(self.fsCheckBtn)
+        self.otherBox.add(self.useLavdoptsBtn)
+        self.threadsBox.add(self.threadsLabel)
+        self.threadsBox.add(self.threadsSpinBtn)
+        self.otherBox.add(self.threadsBox)
+        self.mainBox.add(self.otherBox)
+        #Play btn
+        self.playBtn = Gtk.Button("Play")
+        self.mainBox.add(self.playBtn)
+        self.connect_interface()
+
+    def connect_interface(self):
+        self.chooseFileBtn.connect("clicked",
+                                   self.on_open_file_button_clicked)
+        self.useLavdoptsBtn.connect("toggled",
+                                       self.on_use_lavdopts_toggle)
+        self.fsCheckBtn.connect("toggled",
+                                self.on_fullscreen_toggle)
+        self.threadsSpinBtn.connect("value-changed",
+                                    self.on_spin_button_value_changed)
+        self.playBtn.connect("clicked",
+                             self.on_play_button_clicked)
+        
         
     def set_combo_box(self, comboBox, entries):
         for entry in entries:
@@ -64,18 +98,27 @@ class AMPLauncher(Gtk.Window):
         output = out.decode("utf-8")
         output = output.replace("\t", " ")
         index = output.find(":")
-        vo = str.split(output[index+2:], '\n')
-        return vo[:-2]
+        o = str.split(output[index+2:], '\n')
+        if opt == "vo":
+            self.vo = o[:-2]
+        elif opt == "ao":
+            self.ao = o[:-2]
+        return o[:-2]
+
+    def get_vo_ao_value(self, opt):
+        if opt == "vo":
+            value = self.vo[self.voComboBox.get_active()]
+        elif opt == "ao":
+            value = self.ao[self.aoComboBox.get_active()]
+        index = value[2:].find(" ")+2
+        return value[1:index]
 
     def get_threads(self):
         args = ["nproc", "--all"]
         out, err = Popen(args, stdout=PIPE).communicate()
         return int(out.decode("utf-8"))
-
-    def on_fullscreen_toggle(self, button):
-        pass
     
-    def on_open_file_clicked(self, widget):
+    def on_open_file_button_clicked(self, widget):
         dialog = Gtk.FileChooserDialog("Please choose a video file",
                                        self,
                                        Gtk.FileChooserAction.OPEN,
@@ -84,15 +127,37 @@ class AMPLauncher(Gtk.Window):
                                         Gtk.STOCK_OPEN,
                                         Gtk.ResponseType.OK))
         self.add_filters(dialog)
-
         response = dialog.run()
         if response == Gtk.ResponseType.OK:
-            self.file_path.set_text(dialog.get_filename())
+            self.filePathEntry.set_text(dialog.get_filename())
         elif response == Gtk.ResponseType.CANCEL:
             print("Cancel clicked")
-
         dialog.destroy()
 
+    def on_use_lavdopts_toggle(self, button):
+        isSensitive = self.threadsSpinBtn.get_property('sensitive')
+        if isSensitive:
+            self.threadsSpinBtn.set_sensitive(False)
+            self.lavdoptsToggle = False
+        else:
+            self.threadsSpinBtn.set_sensitive(True)
+            self.lavdoptsToggle = True
+        
+    def on_fullscreen_toggle(self, button):
+        if self.fullscreenToggle:
+            self.fullscreenToggle = False
+        else:
+            self.fullscreenToggle = True
+
+    def on_spin_button_value_changed(self, button):
+        self.lavdoptsThreads = self.threadsSpinBtn.get_value()
+
+    def on_play_button_clicked(self, button):
+        args = ["mplayer", '"' + self.filePathEntry.get_text() + '"',
+                "-ao", self.get_vo_ao_value("ao"),
+                "-vo", self.get_vo_ao_value("vo")]
+        print(args)
+        
     def add_filters(self, dialog):
         filter_all = Gtk.FileFilter()
         filter_all.set_name("All files")
